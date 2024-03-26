260ebbbce9b20eea9165ddff2c1b26cfda86e1c0
ArtFiorindo <alopesfiorindo@gmail.com>
2024-03-25 22:46:39 -0300

    Adicionando img botao BlinCo

--- SalesForce_Challenger/src/components/header/Cabecalho.tsx
+++ SalesForce_Challenger/src/components/header/Cabecalho.tsx
@@ -3,6 +3,7 @@ import './Cabecalho.css';
 import searchIcon from './assets/search-icon.svg';
 import salesIcon from './assets/salesforce-256.png';
 import loginIcon from './assets/account_avatar_people_profile_user_icon_123297.svg';
+import BlindCo from './assets/BlindCo.svg'
 
 interface ClearHeaderProps {
     title: string;
@@ -14,13 +15,11 @@ const ClearHeader: React.FC<ClearHeaderProps> = ({ title }) => {
     return (
         <header className="clear-header">
             <a href='home'><img src={salesIcon} alt='Icone SalesForce' className='sales-icon' /></a>
-            <div className="header-links">
-                <a href=''><p>Cadastro para contato</p></a>
-                <a href='produto'><p>Custom 360</p></a>
-                <a><p>Suporte</p></a>
-                <a><p>Organograma</p></a>
-            </div>
+            
             <div className="header-content">
+            <div className='imgBlind'>
+            <img src={BlindCo} alt='icon-BlindCo'/>
+            </div>
                 <div className="search-login-section">
                     <button className="search-button" onClick={() => setShowSearch(!showSearch)}>
                         <img src={searchIcon} alt="Ícone de pesquisa" />
