3579f23120dbd4c8403aa360916ee5ab49543443
Eduardo Função <45571086+eduardofuncao@users.noreply.github.com>
2024-03-29 23:53:47 -0300

    considera scroll da pagina para mapa de calor

---
+++ blindco/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import SendClicks from './components/SendClicks';
+
+const App: React.FC = () => {
+ 
+
+ return (
+   <>
+   <SendClicks></SendClicks>
+   <p style={{height:1200}}></p>
+   </>
+ );
+};
+
+export default App;
